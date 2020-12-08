5ad2a3a02bf49db928b114232b5aa0d436fa4072
Gabriel Lee <gabrielleeyj@outlook.com>
2020-12-08 12:09:18 +0800

    deprecated code

--- db/connection.go
+++ db/connection.go
@@ -16,15 +16,7 @@ import (
 
 // ConnectDB : helper function to connect to mongoDB database
 func ConnectDB() *mongo.Collection {
-	// // Set client connection
-	// clientOptions := options.Client().ApplyURI("mongodb+srv://Kayige:Kayige26387@kiasibot.egzpq.mongodb.net/db?retryWrites=true&w=majority")
-
-	// // Start connection
-	// client, err := mongo.Connect(context.TODO(), clientOptions)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
+	// main code to start connection
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://Kayige:Kayige26387@kiasibot.egzpq.mongodb.net/db?retryWrites=true&w=majority"))
@@ -37,16 +29,19 @@ func ConnectDB() *mongo.Collection {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(err)
 
-	// check database connection status
+	// check available databases and prints output
 	databases, err := client.ListDatabaseNames(context.TODO(), bson.M{})
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(databases)
 
+	// No errors show success message
 	fmt.Println("Connected to MongoDB Database!")
 
+	// Connect to MongoDB collection to store data.
 	collection := client.Database("db").Collection("usr")
 	return collection
 }
