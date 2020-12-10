5e153d4fc3e225eabd2f0253a19aec04a9f8d01b
Gabriel Lee <gabrielleeyj@outlook.com>
2020-12-10 16:57:00 +0800

    added comments

--- telebot/callbacks.go
+++ telebot/callbacks.go
@@ -2,13 +2,16 @@ package telebot
 
 import tb "gopkg.in/tucnak/telebot.v2"
 
+// makeButtons calls the function within tbot to create buttons for Telegram Chat
 func makeButtons() *tb.ReplyMarkup {
 
+	// shareLoc - Button to share location information
 	shareLoc := tb.ReplyButton{
 		Text:     "Share Location?",
 		Location: true,
 	}
 
+	// getMap - returns URL of the Populated Map Data via Handler.
 	getMap := tb.ReplyButton{
 		Text: "Get Map",
 	}
