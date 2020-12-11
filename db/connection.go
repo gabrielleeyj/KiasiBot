3d1151b042c53b612c7cae790ec232872e73d740
Gabriel Lee <gabrielleeyj@outlook.com>
2020-12-11 11:15:30 +0800

    updated connection codebase

--- db/connection.go
+++ db/connection.go
@@ -13,6 +13,21 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+type ConnectionRepository interface {
+	Session() (*mongo.Client, error)
+	Database(name string) (*mongo.Database, error)
+	Collection(name string) (*mongo.Collection, error)
+}
+type MongoDB struct {
+	Client *mongo.Client
+	DB     *mongo.Database
+	Coll   *mongo.Collection
+}
+
+type Connection struct {
+	app *MongoDB
+}
+
 func init() {
 	err := godotenv.Load()
 	if err != nil {
@@ -20,22 +35,50 @@ func init() {
 	}
 }
 
+func Connect(database, collection string) (*mongo.Collection, error) {
+	// initialize the client connection.
+	conn := NewSession()
+	c, err := conn.Session()
+	if err != nil {
+		return nil, err
+	}
+
+	// initalize the database
+	newdb := NewDatabase(c)
+	db, err := newdb.Database(database)
+	if err != nil {
+		return nil, err
+	}
+
+	// initalize the collection
+	newcollection := NewCollection(db)
+	coll, err := newcollection.Collection(collection)
+	if err != nil {
+		return nil, err
+	}
+	return coll, nil
+}
+
+func NewSession() *MongoDB {
+	return &MongoDB{}
+}
+
 // ConnectDB : helper function to connect to mongoDB database
-func ConnectDB() (*mongo.Collection, error) {
+func (m *MongoDB) Session() (*mongo.Client, error) {
 	// main code to start connection
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("DB_URI")))
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	// ping cluster to check connection status
 	err = client.Ping(context.TODO(), readpref.Primary())
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	fmt.Println(err)
+	fmt.Println("Ping:", err)
 
 	// check available databases and prints output
 	// databases, err := client.ListDatabaseNames(context.TODO(), bson.M{})
@@ -45,9 +88,29 @@ func ConnectDB() (*mongo.Collection, error) {
 	// fmt.Println(databases)
 
 	// No errors show success message
-	fmt.Println("Connected to MongoDB Database!")
+	fmt.Println("Connected to MongoDB Client!")
 
 	// Connect to MongoDB collection for data storage
-	collection := client.Database("db").Collection("usr")
+	// database := client.Database("db")
+	return client, nil
+}
+
+func NewDatabase(client *mongo.Client) ConnectionRepository {
+	return &MongoDB{Client: client}
+}
+func (m *MongoDB) Database(name string) (*mongo.Database, error) {
+	db := m.Client.Database(name)
+	fmt.Println("Connected to Datbase: ", name)
+	return db, nil
+}
+
+func NewCollection(db *mongo.Database) ConnectionRepository {
+	return &MongoDB{DB: db}
+}
+
+func (m *MongoDB) Collection(name string) (*mongo.Collection, error) {
+	db := m.DB
+	collection := db.Collection(name)
+	fmt.Println("Connected to Collection: ", name)
 	return collection, nil
 }
--- db/connection_test.go
+++ db/connection_test.go
@@ -1,14 +1,70 @@
 package db
 
 import (
+	"fmt"
 	"log"
 	"testing"
 )
 
 func TestConnection(t *testing.T) {
-	c, err := ConnectDB()
+	connection, err := Connect("db", "usr")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("%v", c)
+	fmt.Println(connection)
+}
+
+func TestSession(t *testing.T) {
+	// initialize the client connection.
+	conn := NewSession()
+	c, err := conn.Session()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(c)
+}
+
+func TestDatabase(t *testing.T) {
+	// initialize the client connection.
+	conn := NewSession()
+	c, err := conn.Session()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(c)
+
+	// initalize the database
+	newdb := NewDatabase(c)
+	db, err := newdb.Database("db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(db)
+}
+
+func TestCollection(t *testing.T) {
+	// initialize the client connection.
+	conn := NewSession()
+	c, err := conn.Session()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(c)
+
+	// initalize the database
+	newdb := NewDatabase(c)
+	db, err := newdb.Database("db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(db)
+
+	// initalize the collection
+	newcollection := NewCollection(db)
+	coll, err := newcollection.Collection("usr")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(coll)
 }
